1f99e978a8022202f983e64cae1cff0b984ef2db
pere5 <pere5@kth.se>
2017-01-29 20:57:46 +0100

    Adding string comparison algorithms

--- src/main/java/integrator/Parser.java
+++ src/main/java/integrator/Parser.java
@@ -1,17 +1,26 @@
 package integrator;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Per Eriksson on 2017-01-29.
  */
 public class Parser {
+
+    private static Map<String, List<String>> categoryIdMap = null;
+    private static Map<String, List<String>> categoryChildrenMap = null;
+
+    public static Set<String> getMatchingCategoriesSet(String categoryToMatch) {
+
+
+
+        return null;
+    }
 
     public static void parse() {
 
@@ -23,10 +32,31 @@ public class Parser {
 
         String content = readFile();
 
-        Map<String, List<String>> categoryIdMap = Parser.buildCategoryIdMap(content);
-        Map<String, List<String>> categoryChildrenMap = Parser.buildCategoryChildrenMap(categoryIdMap);
-        System.out.println(content);
-        System.out.println(Utils.prettyMap(categoryChildrenMap));
+        categoryIdMap = buildCategoryIdMap(content);
+        categoryChildrenMap = buildCategoryChildrenMap(categoryIdMap);
+        //System.out.println(content);
+        //System.out.println(Utils.prettyMap(categoryChildrenMap));
+
+        /*
+         * Levenshtein distance
+         * This is the number of changes needed to change one String into another,
+         * where each change is a single character modification (deletion, insertion or substitution).
+        */
+        //System.out.println(StringUtils.getLevenshteinDistance("abcdd", "abcf"));                  // high bad
+
+        /*
+         * Jaro Winkler Distance
+         * The Jaro measure is the weighted sum of percentage of matched characters from each file and transposed characters.
+         */
+        //System.out.println(StringUtils.getJaroWinklerDistance("abcd", "abcf"));                         // high good
+
+        /*
+         * Fuzzy Distance
+         * This string matching algorithm is similar to the algorithms of editors such as Sublime Text, TextMate, Atom and others.
+         * One point is given for every matched character. Subsequent matches yield two bonus points.
+         * A higher score indicates a higher similarity.
+         */
+        //System.out.println(StringUtils.getFuzzyDistance("abcd", "abcf", Locale.ENGLISH));   // high good
     }
 
     private static String readFile() {
@@ -72,7 +102,6 @@ public class Parser {
             }
             categoryIdMap.put(id, categories);
         }
-
         return categoryIdMap;
     }
 }
